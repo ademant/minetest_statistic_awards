1f43deeadf6f907eead536b8b1088193de704194
A. Demant <ademant@ademant.de>
2018-12-23 10:18:24 +0100

    integrate import settingtypes.txt

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@ statistic_awards  = {}
 statistic_awards.path = minetest.get_modpath("statistic_awards")
 statistic_awards.modname = minetest.get_current_modname()
 
+-- import settingtypes.txt
+basic_functions.import_settingtype(statistic_awards.path .. "/settingtypes.txt")
+
 dofile(statistic_awards.path .. "/config.lua")
 dofile(statistic_awards.path .. "/awards.lua")
 dofile(statistic_awards.path .. "/abm.lua")
