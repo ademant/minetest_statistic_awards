83783fb92cd19803259c616c92765080ae27bfc9
ademant <ademant@ademant.de>
2019-02-14 13:59:30 +0100

    add german translation

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 statistic_awards  = {}
 statistic_awards.path = minetest.get_modpath("statistic_awards")
 statistic_awards.modname = minetest.get_current_modname()
+local S = dofile(statistic_awards.path .. "/intllib.lua")
+statistic_awards.intllib = S
 
 -- import settingtypes.txt
 basic_functions.import_settingtype(statistic_awards.path .. "/settingtypes.txt")
